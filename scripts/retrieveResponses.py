b6e3b76e77c93848e260ebb91431d65816b19b0c
HaAI Labs <121819295+haailabs@users.noreply.github.com>
2023-01-09 19:30:11 +0700

    Update retrieveResponses.py

--- scripts/retrieveResponses.py
+++ scripts/retrieveResponses.py
@@ -1,21 +1,334 @@
-import json
 from web3 import Web3
+import pandas as pd
 
 # Connect to the Ethereum network
-w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/your-api-key"))
+w3 = Web3(Web3.HTTPProvider("https://polygon-mumbai.g.alchemy.com/v2/kwNMLxtwAtXS_9HN_siSUDMgcCQqDZF5"))
 
 # Create a new account
-account = w3.eth.account.create()
+account = w3.eth.account.create('YourPassword!@')
 
 # Print the address and private key of the new account
 print(f"Address: {account.address}")
 print(f"Private key: {account.privateKey.hex()}")
 
 # Set the address of the DAOkit contract
-contract_address = "0x0123456789abcdef"
+contract_address = "0x4567026fD283ee95421B0271dd3cA3E6daa73f4b"
 
 # Set the ABI (Application Binary Interface) of the contract
-contract_abi = []
+contract_abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_tokenContract",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "_fees",
+				"type": "uint256[]"
+			}
+		],
+		"name": "initialize",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "requestPayment",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "enum DAOkit.types",
+				"name": "_HIPType",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_numOptions",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_numClasses",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_duration",
+				"type": "uint256"
+			}
+		],
+		"name": "submitHIP",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_proposer",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "_response",
+				"type": "uint256[]"
+			}
+		],
+		"name": "submitResponse",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "_number",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "_balance",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "i",
+				"type": "uint256"
+			}
+		],
+		"name": "getFee",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "_fee",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_proposer",
+				"type": "address"
+			}
+		],
+		"name": "getHIPCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "_count",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getNumProposers",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "_numProposers",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "i",
+				"type": "uint256"
+			}
+		],
+		"name": "getProposer",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "_proposer",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_proposer",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_indexHIP",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_indexResponse",
+				"type": "uint256"
+			}
+		],
+		"name": "getResponse",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "_response",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "HIPs",
+		"outputs": [
+			{
+				"internalType": "enum DAOkit.types",
+				"name": "HIPType",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numOptions",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numClasses",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "creationDate",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "duration",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numResponses",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "responded",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "responseRefs",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "proposer",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
 # Load the contract
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
@@ -23,11 +336,9 @@ contract = w3.eth.contract(address=contract_address, abi=contract_abi)
 # Set the address of the retriever
 retriever_address = account.address
 
-# Unlock the account of the retriever
-w3.eth.personal.unlockAccount(retriever_address, "your-account-password")
 
 # Set the address of the proposer
-proposer_address = "0x0123456789abcdef"
+proposer_address = "0x3E960E93B811D5f6727eeBe48eA5bB6A73639306"
 
 # Set the index of the HIP
 index = 0
