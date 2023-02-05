4f689e0e3dc5aacd36ae7283086fe1b6bb140bc3
HaAI Labs <121819295+haailabs@users.noreply.github.com>
2023-02-05 18:25:12 +0700

    Create PeerReview.sol

---
+++ contracts/PeerReview.sol
@@ -0,0 +1,240 @@
+// SPDX-License-Identifier: CC BY 4.0
+pragma solidity ^0.8.12;
+/**
+* @title Peer Review contract
+* @author Nassim Dehouche
+*/
+import "@openzeppelin/contracts/interfaces/IERC721.sol";
+contract PeerReview {
+    // Address of the contract owner
+    address owner;
+    // Address of the ERC-721 contract used to vet respondents
+    address tokenContract ;
+    // Number of proposers
+    uint numProposers;  
+    // Array of proposer addresses
+    address[] proposers;
+    // Fee for submission of a manuscript
+    uint public fee; 
+
+    // Constructor that sets the owner address to the msg.sender
+    constructor() {
+    owner = msg.sender;   
+    }
+
+    /**
+    * @dev Initializes the contract with the ERC-721 contract address and fees
+    * @param _tokenContract is the address of the ERC-721 contract to vet respondents
+    * @param _fee is the submission fee
+    * We assume one address, one NFT, one response. 
+    */
+    function initialize (address _tokenContract, uint _fee )
+    public {
+        // Only the owner can initialize the contract
+        require(msg.sender == owner);
+        // Set the ERC-721 contract address
+        tokenContract = _tokenContract;
+        // Set the fees
+        fee = _fee;
+    }
+
+
+    // The HIP structure
+    struct HIP { 
+        // Creation date
+        uint creationDate;
+        // Duration
+        uint duration;
+        // Number of responses
+        uint numResponses;
+        // Hash of PDF file
+        bytes32 pdfHash;
+        // Hash of request
+        bytes32 requestHash;
+    }
+
+    // Mapping proposers with an array of their proposed HIPs
+    mapping(address => HIP[]) public HIPs; 
+
+    // The Response struct for the content of the response.
+    struct Response{ 
+        // Address of the respondent
+        address respondent;
+        // Classification
+        uint response;
+        // Hash of the response argumentation
+        bytes32 responseHash;
+    }
+
+    // The Response reference struct for payment.
+    struct ResponseRef{ 
+        // Address of the proposer
+        address proposer;
+        // Index of the HIP
+        uint index;
+    }
+
+    // Responses. The first key is the proposer address
+    mapping(address => mapping(uint => Response[])) internal responses;
+
+    // The Response boolean. The first key is the respondent address
+    mapping(address => mapping(address => mapping (uint =>bool))) public responded;
+
+    // The Response reference for payment. Mapping respondent with the HIPs they responded to.
+    mapping(address => ResponseRef[]) public responseRefs;
+
+    // Modifier to check if the user has paid the right fee for the HIP type
+    modifier onlyIfPaidEnough() {
+        require(msg.value==fee, "User did not pay the right fee.");
+        _;
+    }
+
+    // Modifier to check if the user holds the required NFT
+    modifier onlyIfHoldsNFT(address _voter) {
+        require(IERC721(tokenContract).balanceOf(_voter) > 0, "User does not hold the right NFT.");
+        _;
+    } 
+
+    // Modifier to check if the user has already responded to this HIP
+    modifier onlyIfHasNotResponded(address _proposer, uint _id) {
+        require(responded[msg.sender][_proposer][_id]==false, "User has already responded.");
+        _;
+    } 
+
+    // Modifier to check if the HIP is still open for responses
+    modifier onlyIfStillOpen(address _proposer, uint _id) {
+        require(block.timestamp<=HIPs[_proposer][_id].creationDate+HIPs[_proposer][_id].duration, "This HIP is no longer open for responses.");
+        _;
+    } 
+    
+    /**
+    * @dev Submits a HIP
+    * @param _duration is the duration of the HIP
+    * @return _id is the index of the HIP in the proposer's HIP array
+    */
+  
+    function submitHIP(uint _duration, bytes32 _pdfHash, bytes32 _requestHash) 
+    public 
+    payable
+    onlyIfPaidEnough()
+    returns(uint _id){
+        // Set the index of the HIP in the proposer's HIP array
+        _id= HIPs[msg.sender].length;
+        // If this is the proposer's first HIP, increment the number of proposers and add the proposer's address to the proposers array
+        if (_id==0){
+            numProposers++;
+            proposers.push(msg.sender);    
+        }
+        // Add the HIP to the proposer's HIP array
+        HIPs[msg.sender].push();
+        // Set the HIP's properties
+        HIPs[msg.sender][_id].creationDate = block.timestamp;
+        HIPs[msg.sender][_id].duration = _duration;
+        HIPs[msg.sender][_id].pdfHash = _pdfHash;
+        HIPs[msg.sender][_id].requestHash = _requestHash;
+        return _id;
+    }
+
+    /**
+    * @dev Submits a response to a HIP
+    * @param _proposer is the address of the proposer of the HIP
+    * @param _id is the index of the HIP among the proposer's
+    * @param _response is the submitted reponse array
+    */
+    function submitResponse(address _proposer, uint _id, uint _response, bytes32 _responseHash) 
+    public 
+    onlyIfHoldsNFT(msg.sender)
+    onlyIfHasNotResponded(_proposer, _id)
+    onlyIfStillOpen(_proposer, _id)
+    returns(uint _number)
+    {
+        // Check if the response is valid
+        if(_response>3) { revert('Invalid response'); }
+        // Set the index of the response in the proposer's response array
+        _number=responses[_proposer][_id].length+1;
+        // Increment the number of responses for the HIP
+        HIPs[_proposer][_id].numResponses=_number;
+        // Add the response to the proposer's response array
+        responses[_proposer][_id].push();
+        // Set the respondent's address
+        responses[_proposer][_id][_number-1].respondent=msg.sender;
+        // Set the response
+        responses[_proposer][_id][_number-1].response=_response;
+        responses[_proposer][_id][_number-1].responseHash=_responseHash; 
+        // Create a ResponseRef struct for payment
+        ResponseRef memory r;
+        r.proposer = _proposer;
+        r.index = _id;
+        // Add the ResponseRef struct to the respondent's responseRefs array
+        responseRefs[msg.sender].push(r);
+        // Set the respondent's responded boolean for this HIP to true
+        responded[msg.sender][_proposer][_id]=true;
+        return _number;
+    }
+
+    /**
+    * @dev Respondents payment request function for a particular HIP
+    */
+    function requestPayment() public 
+    {
+        uint _balance;
+        uint _id;
+        address _proposer;
+        for (uint i=0;i<responseRefs[msg.sender].length;){
+            _proposer=responseRefs[msg.sender][i].proposer;
+            _id=responseRefs[msg.sender][i].index; 
+            if (_proposer!=address(0) && block.timestamp>HIPs[_proposer][_id].creationDate+HIPs[_proposer][_id].duration){
+        responseRefs[msg.sender][i].proposer=address(0);        
+        _balance+=fee/HIPs[_proposer][_id].numResponses;
+            }
+            unchecked{i++;}
+        }
+      (bool sent, ) = msg.sender.call{value: _balance}("");
+      require(sent, "Failed to send Ether");
+   }
+
+    /**
+    * @dev Returns the total number of proposers
+    */
+    function getNumProposers() public view returns(uint _numProposers){
+        return numProposers;
+    }
+
+    /**
+    * @dev Returns the i-th proposer for indexing
+    */
+    function getProposer(uint i) public view returns(address _proposer){
+        return proposers[i];
+    }  
+
+    /**
+    * @dev Returns the total number of HIPs for indexing
+    */
+    function getHIPCount(address _proposer) public view returns(uint _count){
+     return HIPs[_proposer].length;
+    }
+
+    /**
+    * @dev Returns a particular response to a HIP
+    */
+    function getResponse(address _proposer, uint _indexHIP, uint _indexResponse) public view returns(uint response, bytes32 responseHash){
+        return (responses[_proposer][_indexHIP][_indexResponse].response, responses[_proposer][_indexHIP][_indexResponse].responseHash);
+    }
+    
+    /**
+    * @dev Returns the balance of msg.sender
+    */
+    function getBalance() public view returns(uint _balance){
+        uint _id;
+        address _proposer;
+        for (uint i=0;i<responseRefs[msg.sender].length;){
+        _proposer=responseRefs[msg.sender][i].proposer;
+        _id=responseRefs[msg.sender][i].index; 
+        if (_proposer!=address(0) && block.timestamp>HIPs[_proposer][_id].creationDate+HIPs[_proposer][_id].duration){    
+            _balance+=fee/HIPs[_proposer][_id].numResponses;
+            }
+            unchecked{i++;}
+        }
+        return _balance;
+    }
+    }
