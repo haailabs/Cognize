c83bd34d0e1ce8ebb02cd73b93e531c552335188
HaAI Labs <121819295+haailabs@users.noreply.github.com>
2023-08-23 18:06:39 +0700

    Add files via upload

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Cognize is ERC20, Ownable {
+    constructor() ERC20("Cognize", "CGN") {}
+
+    function mint(address to, uint256 amount) public  {
+        _mint(to, amount);
+    }
+}
